1589f67cbcf874bcf276e5ae1f7a66cf3215e7cd
cynic <kurisufag1@gmail.com>
2022-08-07 16:27:59 -0400

    fix file uploads

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ class bird_inst():
         ctx["data"]["message"]["text"] = html.unescape(ctx["data"]["message"]["text"])
         self.irc.sendraw(privmsg.build(self.config["irc_nick"], self.config["irc_chan"], ctx["data"]["message"]["name"]+": "+ctx["data"]["message"]["text"]).msg)
         for f in ctx["data"]["message"]["files"]:
-            self.limiter.action(self.irc.sendraw, (privmsg.build(self.config["irc_nick"], self.config["irc_chan"], f"({ctx['name']} uploaded file: {self.httpendpoint}/storage/files/{f['name']})").msg,))
+            self.limiter.action(self.irc.sendraw, (privmsg.build(self.config["irc_nick"], self.config["irc_chan"], f"({ctx['data']['message']['name']} uploaded file: {self.httpendpoint}/storage/files/{f['name']})").msg,))
     def handle_exit(self, ctx): pass
     def handle_enter(self, ctx): pass
     def handle_userLoaded(self, ctx): pass
